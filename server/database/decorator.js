39f5be1bc23eabb6fcc7f26d4083e24d58dc727a
Janelle K Hirano <jaahnelle@gmail.com>
2019-10-24 20:49:30 -1000

    adds all models to decorator

--- server/database/decorator.js
+++ server/database/decorator.js
@@ -1,4 +1,20 @@
+const Image = require("./models/Image");
+const User = require("./models/User");
+const ItemCategory = require("./models/ItemCategory");
+const Item = require("./models/Item");
+const ItemCondition = require("./models/ItemCondition");
+const ItemStatus = require("./models/ItemStatus");
+const UserStatus = require("./models/UserStatus");
+
 module.exports = function(req, res, next) {
-  req.database = {};
+  req.database = {
+    User: User,
+    Image: Image,
+    Item: Item,
+    ItemCategory: ItemCategory,
+    ItemCondition: ItemCondition,
+    ItemStatus: ItemStatus,
+    UserStatus: UserStatus
+  };
   next();
 };
