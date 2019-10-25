b0f14990cf1b446d7c13f0e4257ba4dc1ed5c013
Janelle K Hirano <jaahnelle@gmail.com>
2019-10-24 19:49:04 -1000

    server.js setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-const knex = require("knex");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "hiyee" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
