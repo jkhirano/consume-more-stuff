f0f1683d4e70923ceee126662608738c071602ba
Janelle K Hirano <jaahnelle@gmail.com>
2019-10-24 21:17:47 -1000

    update server with decorator

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const decorator = require("./database/decorator");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-
+app.use(decorator);
 app.get("/", (req, res) => {
   return res.json({ message: "hiyee" });
 });
