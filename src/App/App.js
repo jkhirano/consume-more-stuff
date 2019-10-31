d49ca30f1ad7d07581beb39c5e7ca4446d7b28ee
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-10-31 00:30:53 -1000

    adds Array.map to create form fields and handleInput to map input values to state

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.module.css";
 import HabitList from "../containers/HabitList/HabitList";
-import NavigationComponent from "../containers/NavigationComponent.jsx/NavigationComponent";
+import NavigationComponent from "../containers/NavigationComponent";
+import AddProduct from "../containers/AddProduct";
 
 function App() {
   return (
     <div>
       <NavigationComponent />
       <HabitList />
+      <AddProduct />
     </div>
   );
 }
--- src/containers/AddProduct/AddProduct.jsx
+++ src/containers/AddProduct/AddProduct.jsx
@@ -3,10 +3,75 @@ import React, { Component } from "react";
 class AddProduct extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      mappedFields: {
+        name: "Hello",
+        description: "",
+        manufacturer: "",
+        model: "",
+        price: "",
+        dimensions: ""
+      },
+      unmappedFields: {
+        condition_id: undefined,
+        category_id: undefined
+      }
+    };
   }
+
+  handleInput = event => {
+    const { value, name } = event.target;
+    const state = { ...this.state };
+    state.mappedFields[name] = value;
+    this.setState({ state });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form>
+          <ul>
+            {Object.keys(this.state.mappedFields).map(key => {
+              return (
+                <li>
+                  <label htmlFor={key}>
+                    {key[0].toUpperCase() + key.slice(1) + ": "}
+                  </label>
+                  <input
+                    type="text"
+                    name={key}
+                    value={this.state.mappedFields[key]}
+                    onChange={this.handleInput}
+                  />
+                </li>
+              );
+            })}
+            <li>
+              <label htmlFor="condition">Condition: </label>
+              <select name="condition_id">
+                <option value="1">Chronic</option>
+                <option value="2">Learned</option>
+                <option value="3">Obsessive</option>
+              </select>
+            </li>
+            <li>
+              <label htmlFor="category">Category: </label>
+              <select name="category_id">
+                <option value="1">Work</option>
+                <option value="2">Hygiene</option>
+                <option value="3">Social</option>
+                <option value="4">Home</option>
+              </select>
+            </li>
+            <li>
+              <label htmlFor="url">Image URL: </label>
+              <input type="file " name="url" />
+            </li>
+          </ul>
+          <button onClick={this.testInput}>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
