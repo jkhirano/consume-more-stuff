d5ba86c384ff6beabc5f89526ba7ac9e76a54362
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-11-05 17:56:04 -1000

    changes App.module.css to App.css to make styling components faster, and wraps components in App.js in styled divs

--- src/App/App.js
+++ src/App/App.js
@@ -1,42 +1,48 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { loadHabits } from '../actions';
-import "./App.module.css";
+import { connect } from "react-redux";
+import { loadHabits } from "../actions";
+import "./App.css";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 import HabitList from "../containers/HabitList";
 import NavigationComponent from "../containers/NavigationComponent";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
-      <div>
-        <Header />
-        <NavigationComponent />
-        <HabitList habits={this.props.habits} />
+      <div className="app-container">
+        <div className="Header">
+          <Header />
+        </div>
+        <div className="Navigation">
+          <NavigationComponent />
+        </div>
+        <div className="HabitList">
+          <HabitList habits={this.props.habits} />
+        </div>
+        <div className="footer">Footer</div>
       </div>
-    )
+    );
   }
-
 }
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     habits: state.habits
   };
-}
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     loadHabits: () => {
       return dispatch(loadHabits());
     }
-  }
-}
+  };
+};
 
 App = connect(
   mapStateToProps,
