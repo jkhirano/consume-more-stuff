c4c6ae74b1390e72992da59b19f7cc5979ee4cf0
Janelle K Hirano <jaahnelle@gmail.com>
2019-11-09 16:07:40 -1000

    reorganize sequence of detailedHabit descriptions

--- src/components/DetailedHabit/DetailedHabit.jsx
+++ src/components/DetailedHabit/DetailedHabit.jsx
@@ -7,28 +7,30 @@ let DetailedHabit = function(props) {
     <div className="detailedHabit">
       DetailedHabit Component
       <img src="" alt="" />
-      <div className="name">Name: {props.name}</div>
-      <div className="price">Price: {props.price}</div>
-      <div className="desc">Description: {props.description}</div>
+      <div className="detailedName">{props.name}</div>
+      <div className="detailedPrice">$ {props.price}</div>
+      <span className="title">Description:</span> {props.description}
+      <div className="details">
+        <span className="title">Category: </span> {props.category} <br />
+        <span className="title">Condition:</span> {props.condition}
+        <br />
+        <span className="title">Status:</span> {props.status}
+        <br />
+        <span className="title">Manufacturer:</span> {props.manufacturer}
+        <br />
+        <span className="title">Model:</span> {props.model}
+        <br />
+        <span className="title">Dimensions:</span> {props.dimensions}
+      </div>
       <div className="postInfo">
-        Category: {props.category}
+        Views: {props.view_count}
         <br />
         Email: {props.email}
         <br />
-        Views: {props.view_count}
-        <br />
         Posted on: {props.created_at}
       </div>
-      <ul className="details">
-        <li>Condition: {props.condition}</li>
-        <li>Status: {props.status}</li>
-        <li>Manufacturer: {props.manufacturer}</li>
-        <li>Model: {props.model}</li>
-        <li>Dimensions: {props.dimensions}</li>
-      </ul>
       <button className="button">Add to cart</button>
       <button className="button">X</button>
-      <div>{props.message}</div>
       End of Detailed Habit Component
     </div>
   );
